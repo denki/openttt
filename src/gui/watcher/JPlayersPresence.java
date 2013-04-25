62999f96b29d6341f2f852620862abb39d9e006c
Tobias Denkinger <tobias.denkinger@tu-dresden.de>
2013-04-25 09:58:37 +0200

    fixed issue #10

--- src/gui/watcher/JPlayersPresence.java
+++ src/gui/watcher/JPlayersPresence.java
@@ -36,44 +36,51 @@ public class JPlayersPresence extends Watcher {
 	private JTextField aFilter;
 	private JLabel lFilter;
 	private List<Player> players;
-	
-	public class PlayersSorter implements Comparator<Player>  {
+
+	public class PlayersSorter implements Comparator<Player> {
 		@Override
 		public int compare(Player arg0, Player arg1) {
 			if (arg0.getClub() != null & arg1.getClub() != null) {
 				int clubComparison = arg0.getClub().compareTo(arg1.getClub());
-				if (clubComparison != 0) return clubComparison;
+				if (clubComparison != 0)
+					return clubComparison;
 			}
 			return arg0.getFullName().compareTo(arg1.getFullName());
 		}
 	}
-	
+
 	public JPlayersPresence(Main main) {
 		super(Language.get("presence"), main);
 		players = new ArrayList<Player>();
 		refreshList();
-		
+
 		lFilter = new JLabel(Language.get("search"));
-		
+
 		aFilter = new JTextField("");
 		aFilter.addKeyListener(new KeyListener() {
 			@Override
-			public void keyTyped(KeyEvent arg0) {}
+			public void keyTyped(KeyEvent arg0) {
+			}
+
 			@Override
 			public void keyReleased(KeyEvent arg0) {
 				refresh();
 			}
+
 			@Override
-			public void keyPressed(KeyEvent arg0) {}
+			public void keyPressed(KeyEvent arg0) {
+			}
 		});
-		
-		table = new JTable(getTableCells(),
-				new String[] {Language.get("present"), Language.get("paid"), Language.get("player")});
+
+		table = new JTable(getTableCells(), new String[] {
+				Language.get("present"), Language.get("paid"),
+				Language.get("player") });
 		TableCellEditor df = new DefaultCellEditor(new JCheckBox(""));
 		TableCellRenderer cr = new TableCellRenderer() {
 			@Override
-			public Component getTableCellRendererComponent(JTable table, Object value,
-					boolean isSelected, boolean hasFocus, int row, int column) {
+			public Component getTableCellRendererComponent(JTable table,
+					Object value, boolean isSelected, boolean hasFocus,
+					int row, int column) {
 				if (column < 2) {
 					boolean checked = (Boolean) value;
 					return new JCheckBox("", checked);
@@ -82,7 +89,7 @@ public class JPlayersPresence extends Watcher {
 				return new JLabel(v);
 			}
 		};
-		
+
 		table.getColumnModel().getColumn(0).setCellEditor(df);
 		table.getColumnModel().getColumn(1).setCellEditor(df);
 		table.getColumnModel().getColumn(0).setCellRenderer(cr);
@@ -91,21 +98,29 @@ public class JPlayersPresence extends Watcher {
 		int size = 60;
 		table.getColumnModel().getColumn(0).setMaxWidth(size);
 		table.getColumnModel().getColumn(1).setMaxWidth(size);
-		
+
 		table.addMouseListener(new MouseListener() {
 			@Override
-			public void mouseReleased(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0) {
+			}
+
 			@Override
-			public void mousePressed(MouseEvent arg0) {}
+			public void mousePressed(MouseEvent arg0) {
+			}
+
 			@Override
-			public void mouseExited(MouseEvent arg0) {}
+			public void mouseExited(MouseEvent arg0) {
+			}
+
 			@Override
-			public void mouseEntered(MouseEvent arg0) {}
+			public void mouseEntered(MouseEvent arg0) {
+			}
+
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
 				int column = table.columnAtPoint(arg0.getPoint());
 				int row = table.rowAtPoint(arg0.getPoint());
-				
+
 				switch (column) {
 				case 0:
 					players.get(row).setThere(!players.get(row).isThere());
@@ -113,7 +128,7 @@ public class JPlayersPresence extends Watcher {
 				case 1:
 					players.get(row).setPaid(!players.get(row).isPaid());
 				}
-				
+
 				apply();
 			}
 		});
@@ -136,41 +151,40 @@ public class JPlayersPresence extends Watcher {
 	public void apply() {
 		main.refreshState();
 	}
-	
+
 	@Override
 	public void refresh() {
 		refreshList();
-		if (!players.isEmpty()) {
-			table.setModel(new DefaultTableModel(getTableCells(), 
-					new String[] {Language.get("present"), Language.get("paid"), Language.get("player")}));
-			TableCellEditor df = new DefaultCellEditor(new JCheckBox(""));
-			TableCellRenderer cr = new TableCellRenderer() {
-				@Override
-				public Component getTableCellRendererComponent(JTable table, Object value,
-						boolean isSelected, boolean hasFocus, int row, int column) {
-					if (column < 2) {
-						boolean checked = (Boolean) value;
-						return new JCheckBox("", checked);
-					}
-					String v = (String) value;
-					return new JLabel(v);
+		table.setModel(new DefaultTableModel(getTableCells(), new String[] {
+				Language.get("present"), Language.get("paid"),
+				Language.get("player") }));
+		TableCellEditor df = new DefaultCellEditor(new JCheckBox(""));
+		TableCellRenderer cr = new TableCellRenderer() {
+			@Override
+			public Component getTableCellRendererComponent(JTable table,
+					Object value, boolean isSelected, boolean hasFocus,
+					int row, int column) {
+				if (column < 2) {
+					boolean checked = (Boolean) value;
+					return new JCheckBox("", checked);
 				}
-			};
-			
-			
-			table.getColumnModel().getColumn(0).setCellEditor(df);
-			table.getColumnModel().getColumn(1).setCellEditor(df);
-			table.getColumnModel().getColumn(0).setCellRenderer(cr);
-			table.getColumnModel().getColumn(1).setCellRenderer(cr);
-			table.getColumnModel().getColumn(2).setCellRenderer(cr);
-			int size = 60;
-			table.getColumnModel().getColumn(0).setMaxWidth(size);
-			table.getColumnModel().getColumn(1).setMaxWidth(size);
-		}
-		
+				String v = (String) value;
+				return new JLabel(v);
+			}
+		};
+
+		table.getColumnModel().getColumn(0).setCellEditor(df);
+		table.getColumnModel().getColumn(1).setCellEditor(df);
+		table.getColumnModel().getColumn(0).setCellRenderer(cr);
+		table.getColumnModel().getColumn(1).setCellRenderer(cr);
+		table.getColumnModel().getColumn(2).setCellRenderer(cr);
+		int size = 60;
+		table.getColumnModel().getColumn(0).setMaxWidth(size);
+		table.getColumnModel().getColumn(1).setMaxWidth(size);
+
 		repaint();
 	}
-	
+
 	private void refreshList() {
 		players.clear();
 		players.addAll(main.getTournament().getQualifying().getUnassigned());
@@ -180,28 +194,30 @@ public class JPlayersPresence extends Watcher {
 		filter();
 		Collections.sort(players, new PlayersSorter());
 	}
-	
+
 	private void filter() {
 		if (aFilter != null) {
 			List<Player> toRemove = new ArrayList<Player>();
 			String filter = aFilter.getText();
 			for (Player p : players)
-				if (!matches(p, filter)) toRemove.add(p);
+				if (!matches(p, filter))
+					toRemove.add(p);
 			players.removeAll(toRemove);
 		}
 	}
-	
+
 	private Object[][] getTableCells() {
 		List<Object[]> result = new ArrayList<Object[]>();
-		for (Player p: players) {
-			result.add(new Object[] {p.isThere(), p.isPaid(), p.getFullName()});
+		for (Player p : players) {
+			result.add(new Object[] { p.isThere(), p.isPaid(), p.getFullName() });
 		}
 		return result.toArray(new Object[0][0]);
 	}
-	
+
 	private boolean matches(Player p, String filter) {
 		for (String exp : filter.toLowerCase().split(" "))
-			if (!p.getFullName().toLowerCase().contains(exp)) return false;
+			if (!p.getFullName().toLowerCase().contains(exp))
+				return false;
 		return true;
 	}
 
