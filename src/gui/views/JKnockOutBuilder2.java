396e94bfb9954ae05b57982f91eed88b6dc315e9
Tobias Denkinger <tobias.denkinger@gmx.de>
2014-08-17 22:28:37 +0200

    graphics update and some flexibility for KnockOutBuilder

--- src/gui/views/JKnockOutBuilder2.java
+++ src/gui/views/JKnockOutBuilder2.java
@@ -9,9 +9,12 @@ import gui.templates.View;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -26,11 +29,16 @@ import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
+import javax.swing.JSpinner;
 import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.TransferHandler;
 import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import database.Calculator;
 import database.players.Player;
@@ -41,6 +49,12 @@ public class JKnockOutBuilder2 extends View {
 
 	private List<Player> players;
 	private TreePanel treePan;
+	private JSpinner jOffset;
+	private JSpinner jToPlace;
+	private Qualifying qualifying;
+	private DefaultListModel<Player> mPlayers;
+	private JList<Player> lPlayers;
+	private GridBagConstraints gbc;
 
 	public JKnockOutBuilder2(Main m) {
 		super(m);
@@ -74,14 +88,18 @@ public class JKnockOutBuilder2 extends View {
 	class TreePanel extends JPanel {
 		List<JLabel> labels = new ArrayList<JLabel>();
 		private Map<JLabel, Player> playersMap;
+		private int n;
+		private int OFFSET;
+		private int FACTOR = 30;
 
 		public TreePanel(int num) {
 			super();
 			playersMap = new HashMap<JLabel, Player>();
-			int n = Calculator.nextPowerOfTwo(num);
+			n = Calculator.nextPowerOfTwo(num);
+			OFFSET = (int) (Math.log(n) / Math.log(2)) * FACTOR + 20;
 			setLayout(new GridBagLayout());
 			Border b = BorderFactory.createCompoundBorder(
-					BorderFactory.createEmptyBorder(2, 10, 2, 2),
+					BorderFactory.createEmptyBorder(2, 10, 2, OFFSET),
 					BorderFactory.createLineBorder(getForeground(), 1, true));
 			GridBagConstraints gbc = new GridBagConstraints();
 			gbc.fill = GridBagConstraints.BOTH;
@@ -112,23 +130,47 @@ public class JKnockOutBuilder2 extends View {
 				add(l, gbc);
 				gbc.gridy++;
 			}
+
+		}
+
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+			
+			Graphics2D gr = (Graphics2D) g;
+			int a = 0;
+			List<Point> points = new ArrayList<>();
+			for (int i = 0; i < n; i++){
+				Rectangle bnds1 = labels.get(i).getBounds();
+				Point p1 = new Point(bnds1.x + bnds1.width - OFFSET ,
+						(int) (bnds1.y + .5 * bnds1.getHeight()));
+				points.add(p1);
+			}
+			System.out.println(points.size());
+			while (points.size() >= 2) {
+				List<Point> nPoints = new ArrayList<>();
+				for (int i = 0; i < points.size(); i += 2) {
+					Point p1 = points.get(i);
+					Point p2 = points.get(i+1);
+					nPoints.add(new Point(p1.x, (p1.y + p2.y)/2));
+					gr.drawLine(p1.x + a, p1.y, p1.x + a + FACTOR, p1.y);
+					gr.drawLine(p2.x + a, p2.y, p2.x + a + FACTOR, p2.y);
+					gr.drawLine(p1.x + a + FACTOR, p1.y, p1.x + a + FACTOR, p2.y);
+				}
+				points.clear();
+				points.addAll(nPoints);
+				a += FACTOR;
+			}
 		}
 	}
 
 	@Override
 	public void generateWindow() {
-		JList<Player> lPlayers = new JList<Player>();
-		DefaultListModel<Player> mPlayers = new DefaultListModel<Player>();
+		lPlayers = new JList<Player>();
+		mPlayers = new DefaultListModel<Player>();
 
-		Qualifying qualifying = main.getTournament().getQualifying();
-		int from = 1, to = 2;
-		for (Group g : qualifying.getGroups())
-			players.addAll(g.getPlayersByPlace(from, Math.min(g.getSize(), to)));
+		qualifying = main.getTournament().getQualifying();
 
-		mPlayers.clear();
-		for (Player p : players)
-			mPlayers.addElement(p);
-		lPlayers.setModel(mPlayers);
 		lPlayers.setDragEnabled(true);
 		lPlayers.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		ListCellRenderer<Player> rnd = new ListCellRenderer<Player>() {
@@ -143,7 +185,8 @@ public class JKnockOutBuilder2 extends View {
 						.getColor("List.selectionForeground");
 				cb.setText("<html>" + plr.getFullName() + "<br/> "
 						+ plr.getClub() + "</html>");
-				Border border = BorderFactory.createLineBorder(getForeground(), 1, true);
+				Border border = BorderFactory.createLineBorder(getForeground(),
+						1, true);
 				cb.setBorder(border);
 				if (isSelected) {
 					cb.setOpaque(true);
@@ -168,28 +211,62 @@ public class JKnockOutBuilder2 extends View {
 		lPlayers.setLayoutOrientation(JList.VERTICAL_WRAP);
 		lPlayers.setVisibleRowCount(-1);
 
+		SpinnerModel sm = new SpinnerNumberModel(1, 1, 1000, 1);
+		jOffset = new JSpinner(sm);
+		jOffset.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				refresh();
+			}
+		});
+
+		sm = new SpinnerNumberModel(2, 1, 1000, 1);
+		jToPlace = new JSpinner(sm);
+		jToPlace.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				refresh();
+			}
+		});
+
 		treePan = new TreePanel(players.size());
 
 		setLayout(new GridBagLayout());
 
-		GridBagConstraints gbc = new GridBagConstraints();
+		gbc = new GridBagConstraints();
 
 		gbc.fill = GridBagConstraints.BOTH;
-		gbc.weightx = 2;
+		gbc.weightx = 1;
 		gbc.weighty = 1;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		gbc.gridwidth = 1;
+		gbc.gridwidth = 2;
 		gbc.gridheight = 1;
 		add(lPlayers, gbc);
 
-		gbc.weightx = 3;
+		gbc.weighty = 0;
+		gbc.gridwidth = 1;
+		gbc.gridheight = 1;
+		gbc.gridy = 1;
+		add(jOffset, gbc);
+
 		gbc.gridx = 1;
+		add(jToPlace, gbc);
+
+		gbc.gridx = 2;
+		gbc.gridy = 0;
+		gbc.weightx = 4;
+		gbc.weighty = 1;
+		gbc.gridwidth = 3;
+		gbc.gridheight = 2;
 		add(treePan, gbc);
-		
-//		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, lPlayers, treePan);
-//		add(split);
-//		split.setDividerLocation(.3);
+
+		refresh();
+
+		// JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+		// lPlayers, treePan);
+		// add(split);
+		// split.setDividerLocation(.3);
 	}
 
 	@Override
@@ -199,8 +276,18 @@ public class JKnockOutBuilder2 extends View {
 
 	@Override
 	public void refresh() {
-		// TODO Auto-generated method stub
+		players.clear();
+		for (Group g : qualifying.getGroups())
+			players.addAll(g.getPlayersByPlace((Integer) jOffset.getValue(),
+					Math.min(g.getSize(), (Integer) jToPlace.getValue())));
 
+		mPlayers.clear();
+		for (Player p : players)
+			mPlayers.addElement(p);
+		lPlayers.setModel(mPlayers);
+		remove(treePan);
+		treePan = new TreePanel(players.size());
+		add(treePan, gbc);
 	}
 
 	public void next() {
@@ -215,5 +302,4 @@ public class JKnockOutBuilder2 extends View {
 		}
 		main.getTournament().getKnockOut().setPlayers(plrs);
 	}
-
 }
